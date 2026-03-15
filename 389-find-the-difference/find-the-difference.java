c2870677554be5941baf64231530594a904879d7
Anuroop-Reddy <anuroopreddy07@gmail.com>
2026-03-15 22:56:09 +0530

    Time: 2 ms (62.10%) | Memory: 43 MB (92.20%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,22 +1,28 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-
-        
-        int cnt[]=new int[26];
-        char res=' ';
+        char res=0;
         for(int i=0;i<s.length();i++){
-            cnt[s.charAt(i)-'a']++;
-            cnt[t.charAt(i)-'a']--;
+            res^=s.charAt(i)^t.charAt(i);
         }
 
-        cnt[t.charAt(t.length()-1)-'a']--;
-
-        for(int i=0;i<26;i++){
-            if(cnt[i]==-1){
-                res=(char)('a'+i);
-                break;
-            }
-        }
+        res^=t.charAt(t.length()-1);
         return res;
+        
+        // int cnt[]=new int[26];
+        // char res=' ';
+        // for(int i=0;i<s.length();i++){
+        //     cnt[s.charAt(i)-'a']++;
+        //     cnt[t.charAt(i)-'a']--;
+        // }
+
+        // cnt[t.charAt(t.length()-1)-'a']--;
+
+        // for(int i=0;i<26;i++){
+        //     if(cnt[i]==-1){
+        //         res=(char)('a'+i);
+        //         break;
+        //     }
+        // }
+        // return res;
     }
 }
