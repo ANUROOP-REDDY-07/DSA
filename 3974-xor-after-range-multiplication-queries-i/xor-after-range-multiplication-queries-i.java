51817ed3df768e958d3b33f56151ca68f1f9a23a
Anuroop-Reddy <anuroopreddy07@gmail.com>
2026-04-08 23:02:39 +0530

    Time: 60 ms (57.33%) | Memory: 47.9 MB (52.00%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
@@ -5,10 +5,9 @@ class Solution {
         for(int i=0;i<q;i++){
             int l=queries[i][0];
             int r=queries[i][1];
-            int step=queries[i][2];
             while(l<=r){
                 nums[l]=(int)((nums[l]*1L*queries[i][3])%MOD);
-                l+=step;
+                l+=queries[i][2];
             }
         }
         
