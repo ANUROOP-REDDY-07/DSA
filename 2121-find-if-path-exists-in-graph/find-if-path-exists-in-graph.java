3512bb52d6a40967bc0798cb4846e2d46724dbea
Anuroop-Reddy <anuroopreddy07@gmail.com>
2026-05-18 13:11:07 +0530

    Time: 120 ms (58.60%) | Memory: 286.1 MB (29.06%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void bfs(ArrayList<Integer> graph[],boolean vis[],int source){
+        Queue<Integer> q=new LinkedList<>();
+
+        q.add(source);
+        vis[source]=true;
+
+        while(!q.isEmpty()){
+            int curr=q.remove();
+            for(int i=0;i<graph[curr].size();i++){
+                int dest=graph[curr].get(i);
+                if(!vis[dest]){
+                    q.add(dest);
+                    vis[dest]=true;
+                }
+            }
+        }
+    }
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        ArrayList<Integer> graph[]=new ArrayList[n];
+        boolean vis[]=new boolean[n];
+        for(int i=0;i<n;i++){
+            graph[i]=new ArrayList<>();
+        }
+
+        for(int i=0;i<edges.length;i++){
+            int src=edges[i][0];  
+            int dest=edges[i][1];   
+
+            graph[src].add(dest);
+            graph[dest].add(src);
+
+        }
+
+        bfs(graph,vis,source);
+        
+
+        return vis[destination];
+       
+    }
+}
