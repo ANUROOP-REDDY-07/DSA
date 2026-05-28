b008e9d5e247e491e20f2dc6ce904536b04abccd
Anuroop-Reddy <anuroopreddy07@gmail.com>
2026-05-28 18:31:41 +0530

    Time: 6 ms (73.38%) | Memory: 47.5 MB (12.24%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,27 +1,5 @@
 class Solution {
-     public void topSort(int numCourses,ArrayList<Integer> graph[],int[] indeg,ArrayList<Integer> al){
-        Queue<Integer> q=new LinkedList<>();
-
-        for(int i=0;i<numCourses;i++){
-            if(indeg[i]==0){
-                q.add(i);
-            }
-        }
-
-        while(!q.isEmpty()){
-            int curr=q.remove();
-            al.add(curr);
-            for(int e:graph[curr]){
-                indeg[e]--;
-                if(indeg[e]==0){
-                    q.add(e);
-                }
-            }
-        }
-
-
-    }
-
+     
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
         int indeg[]=new int[numCourses];
@@ -40,7 +18,25 @@ class Solution {
         }
 
         ArrayList<Integer> al=new ArrayList<>();
-        topSort(numCourses,graph,indeg,al);
+       
+        Queue<Integer> q=new LinkedList<>();
+
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0){
+                q.add(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int curr=q.remove();
+            al.add(curr);
+            for(int e:graph[curr]){
+                indeg[e]--;
+                if(indeg[e]==0){
+                    q.add(e);
+                }
+            }
+        }
 
         return al.size()==numCourses;
     }
