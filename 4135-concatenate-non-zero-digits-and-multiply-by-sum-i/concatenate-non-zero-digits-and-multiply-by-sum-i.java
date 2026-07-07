37a711c5655a59f3574afa7b62c61cfdf9df019a
Anuroop-Reddy <anuroopreddy07@gmail.com>
2026-07-07 22:59:24 +0530

    Time: 1 ms (99.85%) | Memory: 42.2 MB (96.71%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -3,18 +3,23 @@ class Solution {
         if(n==0){
             return 0;
         }
-        StringBuilder sb=new StringBuilder();
+        //StringBuilder sb=new StringBuilder();
+        long x=0;
+        int i=1;
         long sum=0;
         while(n>0){
             int rem=n%10;
             if(rem!=0) {
-                sb.append(rem);
+                //sb.append(rem);
                 sum+=rem;
+                x+=rem*i;
+                i=i*10;
+
             }
             n=n/10;
         }
-       String f=sb.reverse().toString();
-       long num=(long)Integer.parseInt(f);
-       return num*sum;
+       //String f=sb.reverse().toString();
+       //long num=(long)Integer.parseInt(f);
+       return x*sum;
     }
 }
