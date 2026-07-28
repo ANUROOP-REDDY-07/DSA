fe338a0c945ceaa7476679b23d1fa0d8bdff120b
Anuroop-Reddy <anuroopreddy07@gmail.com>
2026-07-28 23:25:55 +0530

    Time: 51 ms (44.88%) | Memory: 47.7 MB (50.14%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,29 +1,29 @@
 class Solution {
-    boolean solve(int[] nums,int idx,int target,Boolean dp[][]){
+    // boolean solve(int[] nums,int idx,int target,Boolean dp[][]){
 
-        if(target==0){
-            return true;
-        }
+    //     if(target==0){
+    //         return true;
+    //     }
 
-        if(idx==nums.length || target<0){
-            return false; 
-        }
+    //     if(idx==nums.length || target<0){
+    //         return false; 
+    //     }
 
-        if(dp[idx][target]!= null){
-            return dp[idx][target];
-        }
+    //     if(dp[idx][target]!= null){
+    //         return dp[idx][target];
+    //     }
 
-        boolean skip=solve(nums,idx+1,target,dp);
-        boolean take=false;
+    //     boolean skip=solve(nums,idx+1,target,dp);
+    //     boolean take=false;
 
-        if(nums[idx]<=target){
-            take=solve(nums,idx+1,target-nums[idx],dp);
-        }
+    //     if(nums[idx]<=target){
+    //         take=solve(nums,idx+1,target-nums[idx],dp);
+    //     }
 
-        return dp[idx][target]=skip || take;
+    //     return dp[idx][target]=skip || take;
 
 
-    }
+    // }
     public boolean canPartition(int[] nums) {
         int sum=0;
         for(int i=0;i<nums.length;i++){
@@ -34,7 +34,22 @@ class Solution {
             return false;
         }
         sum/=2;
-        Boolean dp[][]=new Boolean[nums.length][sum+1];
-        return solve(nums,0,sum,dp);
+
+        boolean dp[][]=new boolean[nums.length+1][sum+1];
+
+        dp[nums.length][0]=true;
+
+        for(int i=nums.length-1;i>=0;i--){
+            for(int j=0;j<=sum;j++){
+                boolean skip=dp[i+1][j];
+                boolean take=false;
+                if(nums[i]<=j){
+                    take=dp[i+1][j-nums[i]];
+                }
+                dp[i][j]=skip || take;
+            }
+        }
+
+        return dp[0][sum];
     }
 }
