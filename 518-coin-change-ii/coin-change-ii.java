73bff8994819f822e8cdb1e480b8717a205c4eba
Anuroop-Reddy <anuroopreddy07@gmail.com>
2026-08-25 22:44:02 +0530

    Time: 18 ms (26.87%) | Memory: 58.6 MB (5.24%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,33 +1,48 @@
 class Solution {
-    int solve(int amount,int[] coins,int idx,int dp[][]){
+    // int solve(int amount,int[] coins,int idx,int dp[][]){
 
-        if(amount>0 && idx<0){
-            return 0;
-        }
+    //     if(amount>0 && idx<0){
+    //         return 0;
+    //     }
 
-        if(amount==0){
-            return 1;
-        }
+    //     if(amount==0){
+    //         return 1;
+    //     }
 
-        if(dp[amount][idx]!=-1){
-            return dp[amount][idx];
-        }
-        //skip;
-        int skip=solve(amount,coins,idx-1,dp);
-        int take=0;
+    //     if(dp[amount][idx]!=-1){
+    //         return dp[amount][idx];
+    //     }
+    //     //skip;
+    //     int skip=solve(amount,coins,idx-1,dp);
+    //     int take=0;
 
-        //take
-        if(amount>=coins[idx]){
-            take=solve(amount-coins[idx],coins,idx,dp);
-        }
-        return dp[amount][idx]=skip+take;
-    }
+    //     //take
+    //     if(amount>=coins[idx]){
+    //         take=solve(amount-coins[idx],coins,idx,dp);
+    //     }
+    //     return dp[amount][idx]=skip+take;
+    // }
+
     public int change(int amount, int[] coins) {
-        int dp[][]=new int[amount+1][coins.length];
+        int dp[][]=new int[amount+1][coins.length+1];
 
-        for(int i=0;i<=amount;i++){
-            Arrays.fill(dp[i],-1);
+        for(int j=0;j<=coins.length;j++){
+            dp[0][j]=1;
         }
-        return solve(amount,coins,coins.length-1,dp);
+
+        //dp[i][j]-> represents number of ways  amount i made using first j coins
+
+        for(int i=1;i<=amount;i++){
+            for(int j=1;j<=coins.length;j++){
+                
+                int skip=dp[i][j-1];
+                int take=0;
+                if(i>=coins[j-1]){
+                    take=dp[i-coins[j-1]][j];
+                }
+                dp[i][j]=skip+take;
+            }
+        }
+        return dp[amount][coins.length];
     }
 }
